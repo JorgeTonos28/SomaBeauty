33d469e13d3ea92a666a551b3153e76ae9553fb0
JorgeTonos28 <giorgiotonos@gmail.com>
2025-06-15 12:16:43 -0400

    Update setup.sh

--- .codex/setup.sh
+++ .codex/setup.sh
@@ -6,7 +6,8 @@ apt-get update -qq
 apt-get install -y \
   git curl unzip gnupg \
   php-cli php-mbstring php-xml php-sqlite3 php-pdo-sqlite \
-  php-pdo php-dom php-json php-tokenizer php-curl php-zip php-bcmath
+  php-pdo php-dom php-json php-tokenizer php-curl php-zip php-bcmath \
+  php-gd
 
 # 2. Composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
