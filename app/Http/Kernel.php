60c35332ff0b80e00c8ab8f9f0b64e56e8e652e0
JorgeTonos28 <giorgiotonos@gmail.com>
2025-06-17 23:45:20 -0400

    feat: block mobile access

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,6 +34,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\BlockMobileDevices::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
+        'block.mobile' => \App\Http\Middleware\BlockMobileDevices::class,
     ];
 }
---
+++ resources/views/mobile-warning.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+    <div class="text-center text-lg">
+        <p>Acceso temporalmente restringido para dispositivos móviles y tablets.</p>
+        <p>Por favor, ingresa desde un ordenador de escritorio.</p>
+    </div>
+</x-guest-layout>
